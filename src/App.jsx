644756541e8803b129a3a4f954253e093ec52be4
FatemehYafteh <fatemmehyafteh@gmail.com>
2025-08-05 00:47:02 -0700

    singleHotel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -7,22 +6,24 @@ import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
 import Hotels from "./components/Hotels/Hotels";
 import HotelsProvider from "./components/context/HotelsProvider";
+import SingleHotel from "./components/SingleHotel/SingleHotel";
 
 function App() {
-  return <div>
-<HotelsProvider>
-<Toaster/>
-    <Header/>
-    <Routes>
-      <Route path="/" element={ <LocationList/>}/>
-      <Route path="/hotels" element={<AppLayout />}>
-      <Route index element={<Hotels/>}></Route>
-      <Route path="id" element={<div>single hotle</div>}></Route>
-      </Route>
-    </Routes>
-</HotelsProvider>
-  </div>
+  return (
+    <div>
+      <HotelsProvider>
+        <Toaster />
+        <Header />
+        <Routes>
+          <Route path="/" element={<LocationList />} />
+          <Route path="/hotels" element={<AppLayout />}> 
+            <Route index element={<Hotels />} />
+            <Route path="id" element={<SingleHotel/>} />
+          </Route>
+        </Routes>
+      </HotelsProvider>
+    </div>
+  );
 }
 
 export default App;
-
